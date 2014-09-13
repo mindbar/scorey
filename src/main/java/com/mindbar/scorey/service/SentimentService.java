cf312a730a1a0ef53b01fdf330468115628f5c18
mishadoff <mishadoff@gmail.com>
2014-09-13 16:12:10 +0300

    LingPipe from Clojars

---
+++ src/main/java/com/mindbar/scorey/service/SentimentService.java
@@ -0,0 +1,11 @@
+package com.mindbar.scorey.service;
+
+import com.aliasi.classify.DynamicLMClassifier;
+
+/**
+ * @author mishadoff
+ */
+public class SentimentService {
+    public static void main(String[] args) {
+    }
+}
