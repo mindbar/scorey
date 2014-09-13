0917e330185b5a92fcce2e2b511b7d4d82f16bfb
mishadoff <mishadoff@gmail.com>
2014-09-13 16:30:49 +0300

    Build

--- src/main/java/com/mindbar/scorey/service/SentimentService.java
+++ src/main/java/com/mindbar/scorey/service/SentimentService.java
@@ -1,11 +1,25 @@
 package com.mindbar.scorey.service;
 
 import com.aliasi.classify.DynamicLMClassifier;
+import com.aliasi.lm.NGramProcessLM;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 /**
  * @author mishadoff
  */
 public class SentimentService {
+    private static String[] categories = new String[]{"pos", "tag"};
+
     public static void main(String[] args) {
+        DynamicLMClassifier<NGramProcessLM> classifier =
+                DynamicLMClassifier.createNGramProcess(categories, 8);
+    }
+
+    private static void train() {
+        for (int i = 0; i < categories.length; i++) {
+            String cat = categories[i];
+
+        }
     }
 }
